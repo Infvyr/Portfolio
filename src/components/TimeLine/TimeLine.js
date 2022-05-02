797abdfd5b648c437ae0347c1ff67de286af6caa
Vasile Novatchii <vasile.novatchii@proovia.co.uk>
2022-05-02 20:53:19 +0300

    update text

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 import {
   CarouselButton,
@@ -9,10 +9,15 @@ import {
   CarouselItemImg,
   CarouselItemText,
   CarouselItemTitle,
-  CarouselMobileScrollNode
-} from './TimeLineStyles';
-import {Section, SectionDivider, SectionText, SectionTitle} from '../../styles/GlobalComponents';
-import {TimeLineData} from '../../constants/constants';
+  CarouselMobileScrollNode,
+} from "./TimeLineStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { TimeLineData } from "../../constants/constants";
 
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
@@ -21,39 +26,45 @@ const Timeline = () => {
   const carouselRef = useRef();
 
   const scroll = (node, left) => {
-    return node.scrollTo({ left, behavior: 'smooth' });
-  }
+    return node.scrollTo({ left, behavior: "smooth" });
+  };
 
   const handleClick = (e, i) => {
     e.preventDefault();
 
     if (carouselRef.current) {
-      const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
-      
+      const scrollLeft = Math.floor(
+        carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length)
+      );
+
       scroll(carouselRef.current, scrollLeft);
     }
-  }
+  };
 
   const handleScroll = () => {
     if (carouselRef.current) {
-      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+      const index = Math.round(
+        (carouselRef.current.scrollLeft /
+          (carouselRef.current.scrollWidth * 0.7)) *
+          TimeLineData.length
+      );
 
       setActiveItem(index);
     }
-  }
+  };
 
   // snap back to beginning of scroll when window is resized
   // avoids a bug where content is covered up if coming from smaller screen
   useEffect(() => {
     const handleResize = () => {
       scroll(carouselRef.current, 0);
-    }
+    };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-    }
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
@@ -61,26 +72,33 @@ const Timeline = () => {
       <SectionDivider divider />
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        Skilled and passionate Frontend developer with 4+ years of experience in building user interfaces, website redesign, Wordpress custom themes. Positive reputation for quality work, timely construction, and finishing projects at estimated budget. Love learning new things, so I recently managed to learn React.js and keep learning things like Redux, styled components, Emotion, Ant Design, Material UI and many other React features.
+        Skilled and passionate frontend developer with more than 4yrs of
+        experience in building user interfaces, website redesign even newer UI
+        for CRM’s and SPA's. Love learning new things and progressing alongside
+        ambitious and adaptable people. Positive reputation for quality work,
+        timely construction, and finishing projects at estimated budget.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
           {TimeLineData.map((item, index) => (
             <CarouselMobileScrollNode
               key={index}
-              final={index === TOTAL_CAROUSEL_COUNT - 1}>
+              final={index === TOTAL_CAROUSEL_COUNT - 1}
+            >
               <CarouselItem
                 index={index}
                 id={`carousel__item-${index}`}
                 active={activeItem}
-                onClick={(e) => handleClick(e, index)}>
+                onClick={(e) => handleClick(e, index)}
+              >
                 <CarouselItemTitle>
                   {`${item.year}`}
                   <CarouselItemImg
                     width="208"
                     height="6"
                     viewBox="0 0 208 6"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       fillRule="evenodd"
                       clipRule="evenodd"
@@ -94,7 +112,8 @@ const Timeline = () => {
                         y1="0.5"
                         x2="208"
                         y2="0.500295"
-                        gradientUnits="userSpaceOnUse">
+                        gradientUnits="userSpaceOnUse"
+                      >
                         <stop stopColor="#fff" />
                         <stop
                           offset="0.79478"
@@ -121,7 +140,8 @@ const Timeline = () => {
               onClick={(e) => handleClick(e, index)}
               type="button"
               name={`slide-${index}`}
-              aria-label={`slide ${index} was selected`}>
+              aria-label={`slide ${index} was selected`}
+            >
               <CarouselButtonDot active={activeItem} />
             </CarouselButton>
           );
