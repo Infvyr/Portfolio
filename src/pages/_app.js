945c4aba27e2e584193a590a600f070bdaf3f413
Infvyr <allfortrashmail@gmail.com>
2022-11-17 10:00:36 +0200

    update seo description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,9 +4,6 @@ import withDarkMode, { useDarkMode } from 'next-dark-mode';
 import { lightTheme, darkTheme } from '../themes/default';
 import GlobalStyles from "../styles/globals";
 
-const description = "I'm a passionate front-end developer from Moldova. I create full UI from scratch or using UI libraries, React or websites using Next.js"
-const title = "Vasile Novatchii Projects Portfolio"
-
 function App({ Component, pageProps }) {
   const { darkModeActive } = useDarkMode();
   const theme = darkModeActive ? darkTheme : lightTheme;
@@ -15,14 +12,14 @@ function App({ Component, pageProps }) {
     <>
       <ThemeProvider theme={{darkMode: darkModeActive, ...theme}}>
         <NextSeo
-          title={title}
-          description={description}
+          title="Vasile Novatchii Projects Portfolio"
+          description="I'm a passionate front-end developer from Moldova. I create full UI from scratch or using UI libraries, React or websites using Next.js"
           openGraph={{
             type: 'website',
             locale: 'en_GB',
             url: '',
-            description,
-            site_name: title,
+            description: "I'm a passionate front-end developer from Moldova. I create full UI from scratch or using UI libraries, React or websites using Next.js",
+            site_name: "Vasile Novatchii Projects Portfolio",
           }}
         />
         <GlobalStyles />
